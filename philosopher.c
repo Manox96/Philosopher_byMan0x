7db3ee39bcd1278129a9b1b3345756fb998debf9
@Manox96 <aymanemanox15@gmail.com>
2025-01-12 20:12:27 +0100

    ❤️

--- philosopher.c
+++ philosopher.c
@@ -4,10 +4,11 @@
 int main(int ac, char **av)
 {
 	t_data data;
+	
 	if (ac != 5 && ac != 6)
 		print_err("should be 5 or 4 args");
 	initial_inputs(&data, ac, av);
-	start_sumilation(&data);
+	// start_sumilation(&data);
 	puts("ok");
 
 }
